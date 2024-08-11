427ac4c3aaa145fd7dd3bba0f2f642d7b4fdc5f6
Alkis Skoutaris <askoutaris@novibet.com>
2024-08-11 16:51:43 +0300

    Initial commit

---
+++ ConsumerQueue.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Concurrent;
+
+namespace ProducerConsumerQueue
+{
+	public class ConsumerQueue<T>
+	{
+		private readonly ConcurrentQueue<T> _queue;
+		private readonly Func<T, Task> _onItemConsume;
+		private readonly Func<Exception, T, Task> _onItemError;
+		private readonly AutoResetEvent _sync;
+		private CancellationTokenSource _tokenSource;
+		private Task? _task;
+
+		public ConsumerQueue(Func<T, Task> onItemConsume, Func<Exception, T, Task> onItemError)
+		{
+			_queue = new ConcurrentQueue<T>();
+			_sync = new AutoResetEvent(false);
+			_tokenSource = new CancellationTokenSource();
+			_onItemConsume = onItemConsume;
+			_onItemError = onItemError;
+		}
+
+		public void Enqueue(T item)
+		{
+			_queue.Enqueue(item);
+
+			_sync.Set();
+		}
+
+		private async Task Consume()
+		{
+			_sync.WaitOne();
+
+			while (!_tokenSource.IsCancellationRequested && _queue.TryDequeue(out T? item))
+			{
+				try
+				{
+					await _onItemConsume.Invoke(item);
+				}
+				catch (Exception ex)
+				{
+					await _onItemError(ex, item);
+				}
+			}
+		}
+
+		public void Start()
+		{
+			lock (_sync)
+			{
+				if (_tokenSource.IsCancellationRequested)
+					_tokenSource = new CancellationTokenSource();
+
+				_task = Task.Run(async () =>
+				{
+					while (!_tokenSource.IsCancellationRequested)
+						await Consume();
+				});
+			}
+		}
+
+		public void Stop()
+		{
+			lock (_sync)
+			{
+				_tokenSource.Cancel();
+				_sync.Set();
+				_task?.GetAwaiter().GetResult();
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace ProducerConsumerQueue
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var queue = new ConsumerQueue<string>(onItemConsume, onItemError);
+
+			queue.Start();
+
+			for (int i = 0; i < 5; i++)
+			{
+				var queueIdx = i;
+				Task.Run(async () =>
+				{
+					for (int j = 0; j < 5; j++)
+					{
+						await Task.Delay(1000);
+						queue.Enqueue($"Item {j} from queue {queueIdx}");
+					}
+				});
+			}
+
+			Console.WriteLine("ProducerConsumerQueue Started");
+			Console.ReadLine();
+
+			Console.WriteLine("ProducerConsumerQueue Stopping...");
+			queue.Stop();
+			Console.WriteLine("ProducerConsumerQueue Stopped");
+			Console.ReadLine();
+		}
+
+		private static async Task onItemConsume(string item)
+		{
+			await Task.Delay(50);
+
+			Console.WriteLine(item);
+		}
+
+		private static async Task onItemError(Exception ex, string item)
+		{
+			await Task.Delay(50);
+
+			Console.WriteLine(ex.Message);
+		}
+	}
+}
